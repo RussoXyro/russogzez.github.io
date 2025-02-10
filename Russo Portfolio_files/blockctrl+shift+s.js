9601c799211b856000e82a75cbabb3cc55aba626
RussoXyro <bleachkurusaki120@gmail.com>
2025-02-10 17:30:33 +0100

    Delete Russo Portfolio_files directory

--- Russo Portfolio_files/blockctrl+shift+s.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("contextmenu", (event) => event.preventDefault()); // Blocks right-click
-document.addEventListener("keydown", function (event) {
-  if (event.key === "F12") {
-    event.preventDefault();
-  }
-});
--- Russo Portfolio_files/blockselect.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("selectstart", (e) => e.preventDefault());
-document.addEventListener("contextmenu", (e) => e.preventDefault());
-
-document.getElementById("no-select")?.addEventListener("selectstart", (e) => {
-  e.preventDefault();
-});
